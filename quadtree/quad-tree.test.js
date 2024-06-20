5e06c0ec015b7aadaf34266565a76b8d785a1f4d
Diniden <diniden@outlook.com>
2024-06-20 15:10:56 -0600

    fixed

--- quadtree/quad-tree.test.js
+++ quadtree/quad-tree.test.js
@@ -3,6 +3,17 @@ import { QuadTree } from "./quad-tree.js";
 
 describe("QuadTree", () => {
   it("Can create a Quad Tree", () => {
-    assert(new QuadTree());
+    assert(new QuadTree(new Bounds(0, 0, 1000, 1000)));
+  });
+
+  it("Will NOT create a QuadTree", () => {
+    try {
+      new QuadTree();
+      assert(false);
+    }
+
+    catch (e) {
+      assert(true);
+    }
   });
 });
