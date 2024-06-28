d1c302d7c5680c01472a6744ed8a29030c79ceb5
Andrew Wysk <andrewwysk@Andrews-MacBook-Air-2.local>
2024-06-28 16:41:21 -0500

    Making pong appear

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Pong } from "./pong/pong.js";
 import { QuadTreeDemo } from "./quadtree/quad-tree-demo.js";
-// const pong = new Pong("game-board");
-const quad = new QuadTreeDemo("game-board");
+const pong = new Pong("game-board");
+// const quad = new QuadTreeDemo("game-board");
